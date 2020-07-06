691960fc648ffc13d4e3fe0d4ac7397ae0012da3
vimalasaranya jandhyala <saranyajandhyala@gmail.com>
2020-07-06 13:02:35 +0530

    fourth commit

---
+++ lib/http_service.dart
@@ -0,0 +1,21 @@
+class HttpService {
+  final String postsURL = "https://jsonplaceholder.typicode.com/posts";
+
+  Future<List<Post>> getPosts() async {
+    Response res = await get(postsURL);
+
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+
+      List<Post> posts = body
+          .map(
+            (dynamic item) => Post.fromJson(item),
+          )
+          .toList();
+
+      return posts;
+    } else {
+      throw "Can't get posts.";
+    }
+  }
+}
